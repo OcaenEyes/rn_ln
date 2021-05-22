14571d008babe81dbd653a36f8cb07e6f58329d9
OcaenEyes <1450136519@qq.com>
2021-05-22 12:01:52 +0800

    master

---
+++ aihub/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MainPage from '../src/components/aiAmis.tsx'
+
+function App() {
+  return (
+    <div className="App">
+      <MainPage></MainPage>
+    </div>
+  );
+}
+
+export default App;
